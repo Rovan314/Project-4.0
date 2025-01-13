64f22434c5e719bef3e66685c8444486bd9a5ea7
Rovan314 <ridwanahtesham@gmail.com>
2025-01-14 00:26:24 +0300

    Update maine.py

--- maine.py
+++ maine.py
@@ -16,17 +16,26 @@ def main():
 
 def sleep():
         print('Focus on nothing but the task at hand for two minutes, because it helps you concentrate and will keep you busy later on')
-        time.sleep(1)
-        taim = int(input('Enter your desired time in minutes:seconds format:'))
-min, sec = map(int, taim.split(':'))
-taim = min*60 + sec
-for x in range(taim, 0, -1):
-     seconds = x % 60
-     minutes = int(x/60) % 60
-     print(f'{minutes:02}:{seconds:02}')
-     time.sleep(1)
-print('Done1')
-
+        time.sleep(5)
+        mins = 0
+        secs = 3
+        taimer = mins * 60 + secs
+        for x in range(taimer, 0, -1):
+            minutes = x // 60
+            seconds = x % 60
+            print(f'{minutes:02}:{seconds:02}')
+            time.sleep(1)
+        print("time's up")
+        choice = input('Do you feel like you can do more? (yes or no): ')
+        if choice.lower() in ['yes', 'y']:
+            print('Excellent!')
+            taimer()
+        elif choice in ['no', 'n']:
+         print('Good luck!')
+         exit()
+        else: 
+         print('It\'s yes or no...')
+        return 
 
 
 def taimer():
@@ -38,6 +47,6 @@ def taimer():
      minutes = int(x/60) % 60
      print(f'{minutes:02}:{seconds:02}')
      time.sleep(1)
-    print('Done1')
+    print('Done')
 
-taimer()
+main()
